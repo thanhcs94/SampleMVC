7ede591dedc897f3cbd897e02e6cc43fb0f59c83
thanhcs94 <nguyenvanthanh9294@gmail.com>
2017-03-02 13:26:12 +0700

    Initial commit

---
+++ app/src/main/java/com/thanhcs/samplemvc/controller/Controller.java
@@ -0,0 +1,9 @@
+package com.thanhcs.samplemvc.controller;
+
+/**
+ * Created by ThanhCS94 on 3/2/17.
+ * I'm HIDING. Don't waste your time to find me.
+ */
+
+public class Controller {
+}
---
+++ app/src/main/java/com/thanhcs/samplemvc/models/ModelProducts.java
@@ -0,0 +1,9 @@
+package com.thanhcs.samplemvc.models;
+
+/**
+ * Created by ThanhCS94 on 3/2/17.
+ * I'm HIDING. Don't waste your time to find me.
+ */
+
+public class ModelProducts {
+}
